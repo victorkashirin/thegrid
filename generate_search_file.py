1acd5ae5593d8278ce370d71fd1b22d4ba4eb1bf
Victor Kashirin <kashirin.victor@gmail.com>
2025-04-14 22:08:16 +0200

    First commit

---
+++ generate_search_file.py
@@ -0,0 +1,49 @@
+import json
+from datetime import datetime
+from PIL import Image
+import os
+import math
+
+MANIFESTS_CACHE = "/Users/victorkashirin/code/Rack/vcv-rack-library/manifests-cache.json"
+
+def get_module_size(plugin_slug, module_slug):
+    try:
+        image_path = f"images/{plugin_slug}/{module_slug}.webp"
+        if os.path.exists(image_path):
+            with Image.open(image_path) as img:
+                width = img.width
+                return math.ceil(width / 15)
+    except Exception as e:
+        print(f"Error processing image for {plugin_slug}/{module_slug}: {e}")
+    return None
+
+with open(MANIFESTS_CACHE, 'r') as f:
+    manifests = json.load(f)
+    
+modules = json.load(open("parsed_plugins.json", "r"))
+
+results = []
+
+for module in modules:
+    plugin_slug = module["plugin_slug"]
+    module_slug = module["module_slug"]
+    timestamp = manifests[plugin_slug]["modules"][module_slug]["creationTimestamp"]
+    module_size = get_module_size(plugin_slug, module_slug)
+    data = [module["plugin_slug"], 
+            module["plugin_name"],
+            module["module_name"],
+            module["module_slug"],
+            module["description"],
+            module["tags"],
+            timestamp,
+            module_size
+    ]
+    results.append(data)
+
+final_result = {
+    "headers": ["plugin_slug", "plugin_name", "module_name", "module_slug", "description", "tags", "timestamp", "size"],
+    "data": results
+}
+
+with open("search_file.json", "w") as f:
+    json.dump(final_result, f)
---
+++ parse_search.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+import json
+import os
+import requests
+import time
+
+folder = "/Users/victorkashirin/code/Rack/vcv-rack-library/manifests"
+
+
+def parse_plugin_json_to_list(path: Path) -> list:
+    """
+    Parses a JSON string representing plugin information and outputs a list of
+    dictionaries, one for each module.
+
+    Args:
+        json_string (str): The JSON string to parse.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary contains the
+              plugin name, plugin slug, module name, module slug, description,
+              and tags for a specific module. Returns an empty list if parsing
+              fails or if there are no modules.
+    """
+    try:
+        data = json.load(open(path, "r"))
+        version = data.get("version")
+        if version < "2.0.0":
+            return []
+        plugin_name = data.get("name")
+        plugin_slug = data.get("slug")
+        modules = data.get("modules", [])
+        output_list = []
+        for module in modules:
+            if module.get("hidden") == True:
+                continue
+            if module.get("deprecated") == True:
+                continue
+            module_info = {
+                "plugin_name": plugin_name,
+                "plugin_slug": plugin_slug,
+                "module_name": module.get("name"),
+                "module_slug": module.get("slug"),
+                "description": module.get("description"),
+                "tags": module.get("tags", []),
+            }
+            output_list.append(module_info)
+        return output_list
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return []
+
+
+def parse_all_plugins(folder: str) -> list:
+    """
+    Parses all JSON files in the specified folder and returns a list of
+    dictionaries containing plugin and module information.
+    Args:
+        folder (str): The folder containing the JSON files.
+    Returns:
+        list: A list of dictionaries, where each dictionary contains the
+              plugin name, plugin slug, module name, module slug, description,
+              and tags for a specific module.
+    """
+    all_plugins = []
+    for path in Path(folder).rglob("*.json"):
+        plugin_list = parse_plugin_json_to_list(path)
+        all_plugins.extend(plugin_list)
+    return all_plugins
+
+
+def download_images():
+      """
+      Downloads images for each plugin and module in the parsed plugins list.
+      Images are saved in the 'images' folder, organized by plugin slug.
+      """
+      with open("parsed_plugins.json", "r") as f:
+        plugins = json.load(f)
+
+      images_folder = Path("images")
+      images_folder.mkdir(exist_ok=True)
+
+      for moduleData in plugins:
+        plugin_slug = moduleData["plugin_slug"]
+        module_slug = moduleData["module_slug"]
+        image_url = f"https://library.vcvrack.com/screenshots/100/{plugin_slug}/{module_slug}.webp"
+        plugin_folder = images_folder / plugin_slug
+        plugin_folder.mkdir(exist_ok=True)
+        image_path = plugin_folder / f"{module_slug}.webp"
+
+        if os.path.exists(image_path):
+           continue
+
+        try:
+          response = requests.get(image_url, timeout=10)
+          response.raise_for_status()
+          with open(image_path, "wb") as img_file:
+            img_file.write(response.content)
+            time.sleep(0.1)
+          print(f"Downloaded: {image_path}")
+        except requests.RequestException as e:
+          print(f"Failed to download {image_url}: {e}")
+
+if __name__ == "__main__":
+    with open('parsed_plugins.json', 'w') as f:
+        json.dump(parse_all_plugins(folder), f, indent=4)
+    # download_images()
